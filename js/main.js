93c30844251e0d288255ab9027a0a0ce5f6be6f1
Zakk Seiman <zseiman@zseiman.com>
2016-05-19 15:36:18 -0400

    structure and interactivity
    completed basic structure and added rotating feature to music/video
    browsing section.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 $(function(){
 	
 $('#mClick').click(function(){
-	//add code to flip to #video
+	$('#mClick').css({'text-shadow':'2px 2px #999'});
+	$('#vClick').css({'text-shadow':'0px 0px'});
+	$('#video').css({'-moz-transform':'rotatey(180deg)','-ms-transform':'rotatey(180deg)','-webkit-transform':'rotatey(180deg)','-o-transform':'rotatey(180deg)'});
+	$('#music').css({'-moz-transform':'rotatey(0deg)','-ms-transform':'rotatey(0deg)','-webkit-transform':'rotatey(0deg)','-o-transform':'rotatey(0deg)'});
 });
 
 $('#vClick').click(function(){
-	//add code to flip #music
+	$('#vClick').css({'text-shadow':'2px 2px #999'});
+	$('#mClick').css({'text-shadow':'0px 0px'});
+	$('#video').css({'-moz-transform':'rotatey(0deg)','-ms-transform':'rotatey(0deg)','-webkit-transform':'rotatey(0deg)','-0-transform':'rotatey(0deg)'});
+	$('#music').css({'-moz-transform':'rotatey(180deg)','-ms-transform':'rotatey(0deg)','-webkit-transform':'rotatey(0deg)','-o-transform':'rotatey(0deg)'});
 });
 
 });
