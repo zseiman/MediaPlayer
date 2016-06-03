85d592cfa707f4f7a4e1b272f95fd526a1a4d65a
Zakk Seiman <zseiman@zseiman.com>
2016-06-03 13:48:01 -0400

    Player buttons and volume(visual only) and visualizer
    Added button and volume slider. (Not yet functional)
    Created canvas visualizer with sample music.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(function(){
 	
 $('#mClick').click(function(){
-	$('#mClick').css({'text-shadow':'2px 2px #999'});
+	$('#mClick').css({'text-shadow':'1px 1px #aea3dc'});
 	$('#vClick').css({'text-shadow':'0px 0px'});
 	$('#video').css({'-moz-transform':'rotatey(180deg)','-ms-transform':'rotatey(180deg)','-webkit-transform':'rotatey(180deg)','-o-transform':'rotatey(180deg)'});
 	$('#music').css({'-moz-transform':'rotatey(0deg)','-ms-transform':'rotatey(0deg)','-webkit-transform':'rotatey(0deg)','-o-transform':'rotatey(0deg)'});
 });
 
 $('#vClick').click(function(){
-	$('#vClick').css({'text-shadow':'2px 2px #999'});
+	$('#vClick').css({'text-shadow':'1px 1px #aea3dc'});
 	$('#mClick').css({'text-shadow':'0px 0px'});
 	$('#video').css({'-moz-transform':'rotatey(0deg)','-ms-transform':'rotatey(0deg)','-webkit-transform':'rotatey(0deg)','-0-transform':'rotatey(0deg)'});
 	$('#music').css({'-moz-transform':'rotatey(180deg)','-ms-transform':'rotatey(0deg)','-webkit-transform':'rotatey(0deg)','-o-transform':'rotatey(0deg)'});
---
+++ js/visualizer.js
@@ -0,0 +1,41 @@
+var analyser, actx, canvas, source, dataArr, bars, draw;
+var audio = document.getElementById('audio');
+audio.src = './music-temp/EverythingSux.m4a';
+audio.controls = false;
+audio.loop = false;
+audio.autoplay = true;
+
+window.addEventListener('load',audioini, false);
+
+function audioini(){
+
+actx = new AudioContext();
+analyser = actx.createAnalyser();
+canvas = document.getElementById("canvas");
+draw = canvas.getContext("2d");
+source = actx.createMediaElementSource(audio);
+source.connect(analyser);
+vis();
+}
+
+function vis(){
+
+	window.requestAnimationFrame(vis);
+	console.log(analyser);
+	dataArr = new Uint8Array(analyser.frequencyBinCount);
+	analyser.getByteFrequencyData(dataArr);
+	analyser.connect(actx.destination);
+	draw.clearRect(0,0, canvas.width, canvas.height);
+	draw.fillStyle = '#0f0';
+	bars = 14;
+	for(var i = 0; i < bars;i++){
+
+		var bloc = i * 20 + 20;
+		var bwid = 10;
+		var bheight = dataArr[i] / -1.9;
+
+		draw.fillRect(bloc, 140, 10, bheight);
+	}
+
+
+}
